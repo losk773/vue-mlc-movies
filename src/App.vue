d8c47e16377512679fd7a757287e9dafc10b448f
losk7 <losk773@gmail.com>
2018-09-09 23:26:39 +0300

    create home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="welcome">
     <div class="welcome__box">
-      <h1 class="weclome__title">My lovel Movie</h1>
+      <!-- <h1 class="welcome__title">My Lovely Movie</h1> -->
+      <img src="./assets/logo.png" alt="My lovely movie">
       <authorization-form></authorization-form>
     </div>
   </div>  
@@ -15,11 +16,46 @@ export default {
   }
 }
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&subset=latin-ext');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
+
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Monoton');
+@import url('https://fonts.googleapis.com/css?family=Fascinate+Inline');
 .welcome {
+  position: relative;
   height: 100vh;
-  background: url(./assets/cover_1.jpg) center center no-repeat;
+  background: url(./assets/cover.jpg) center center no-repeat;
   background-size: cover;
+  &__title {
+    font-family: 'Monoton', cursive;
+    font-size: 125px;
+    font-weight: 700;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+    text-shadow: 0 2px 5px #272727;
+  }
+  &__box {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    padding-top: 80px;
+    text-align: center;
+  }
 }
 </style>
 
--- src/components/AuthorizationForm.vue
+++ src/components/AuthorizationForm.vue
@@ -1,18 +1,13 @@
 <template>
   <form class="login-form">
-    <div class="login-form__head">
-      <h2 class="login-form__title">Sign in</h2>
-    </div>
-    <div class="login-form__field">
-      <input type="text" class="login-form__input">
-    </div>
-    <div class="login-form__field">
-      <input type="password" class="login-form__input">
-    </div>
-    <div class="login-form__field">
-      <button class="login-form__submit">Go</button>
-    </div>
-  </form>  
+      <div class="login-form__field login-form__field_login">
+        <input type="text" class="login-form__input" placeholder="Login">
+      </div>
+      <div class="login-form__field login-form__field_password">
+        <input type="password" class="login-form__input" placeholder="Password">
+      </div>
+        <button class="login-form__submit">Sign in</button>
+  </form>
 </template>
 
 <script>
@@ -21,6 +16,93 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.login-form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  padding: 35px 25px;
+  border-radius: 5px;
+  overflow: hidden;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fcfcfc;
+    opacity: .65;
+    z-index: -1;
+  }
+  &__field {
+    position: relative;
+    overflow: hidden;
+    border-radius: 3px;
+    margin-bottom: 20px;
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      width: 40px;
+      line-height: 40px;
+      text-align: center;
+      font-size: 21px;
+      background-color: #f1f1f1;
+      color: #a59d9d;
+      z-index: 5;
+      font-family: 'Material Icons';
+    }
+    &_login {
+      &:before {
+        content: 'person';
+      }
+    }
+    &_password {
+      &:before {
+        content: 'lock';
+      }
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  &__title {
+    font-size: 21px;
+    font-weight: 900;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    color: #ffffff;
+  }
+  &__input {
+    position: relative;
+    width: 100%;
+    padding: 5px 50px;
+    height: 40px;
+    font-size: 14px;
+    border: none;
+  }
+  &__submit {
+    display: block;
+    min-width: 200px;
+    margin: 0 auto;
+    padding: 15px 0;
+    text-transform: uppercase;
+    font-weight: 500;
+    letter-spacing: 2px;
+    border: none;
+    border-radius: 3px;
+    background-color: #076f8a;
+    color: #ffffff;
+    cursor: pointer;
+    transition: all .4s;
+    &:hover {
+      background-color: #a92929;
+    }
+  }
+}
 </style>
