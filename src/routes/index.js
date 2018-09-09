319e74afbeaf418cc8718748caa71cde535cacd7
losk7 <losk773@gmail.com>
2018-09-10 00:17:03 +0300

    add routes (movies, movie)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,27 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+import Welcome from '../components/Welcome';
+import Movie from '../components/Movie';
+import MovieList from '../components/MovieList';
 
 const routes = [
-
+  {
+    path: '',
+    component: Welcome
+  },
+  {
+    name: 'movies',
+    path: '/movies',
+    component: MovieList,
+    children: [
+      {
+        name: 'movie',
+        path: 'movie',
+        component: Movie
+      }
+    ]
+  }
 ];
 
 export const router = new VueRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,11 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+import movies from './modules/movies';
+
 export const store = new Vuex.Store({
   modules: {
-
+    movies
   },
   strict: process.env.NODE_ENV !== 'production'
 });
