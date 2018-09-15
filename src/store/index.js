65c8482608616bb9261bacf84308b2b1933c032b
losk7 <losk773@gmail.com>
2018-09-15 18:45:14 +0300

    add user state

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,11 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 import movies from './modules/movies';
+import user from './modules/user';
 
 export const store = new Vuex.Store({
   modules: {
+    user,
     movies
   },
   strict: process.env.NODE_ENV !== 'production'
---
+++ src/store/modules/user.js
@@ -0,0 +1,14 @@
+export default {
+  namespaced: true,
+  state: {
+    isAuthorized: false,
+    user: {
+      id: 1,
+      login: 'losk773',
+      password: '123'
+    }
+  },
+  getters: {
+    
+  }
+}
