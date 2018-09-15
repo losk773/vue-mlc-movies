5721af8ef54ff165cb4a2e4816377e4ea8421a5c
losk7 <losk773@gmail.com>
2018-09-15 18:44:28 +0300

    add new routes (home, welcome, movie, movieslist)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,26 +3,36 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+import Home from '../components/Home';
 import Welcome from '../components/Welcome';
 import Movie from '../components/Movie';
 import MovieList from '../components/MovieList';
 
-const routes = [
-  {
+const routes = [{
     path: '',
-    component: Welcome
+    redirect: {
+      name: 'home'
+    }
   },
   {
-    name: 'movies',
-    path: '/movies',
-    component: MovieList,
-    children: [
-      {
+    name: 'home',
+    path: '/home',
+    component: Home,
+    children: [{
+      name: 'movies',
+      path: 'movies',
+      component: MovieList,
+      children: [{
         name: 'movie',
         path: 'movie',
         component: Movie
-      }
-    ]
+      }]
+    }]
+  },
+  {
+    name: 'welcome',
+    path: '/login',
+    component: Welcome
   }
 ];
 
