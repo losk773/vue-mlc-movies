38f2daa47b106cbfd9e870ff6df0a2cfc3e5a3af
losk773 <losk773@gmail.com>
2018-11-28 01:58:59 +0300

    edit config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'dist/',
     filename: 'build.js'
   },
   module: {
@@ -64,7 +64,8 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: '[name].[ext]',
+          outputPath: 'static/'
         }
       }
     ]
