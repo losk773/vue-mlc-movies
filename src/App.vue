ccba2619a6f38bbf37ca9721c9f662e68375deaa
losk7 <losk773@gmail.com>
2018-11-28 00:11:50 +0300

    refactoring code, deleted folder docs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
-  <div>
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
-<script>
-export default {
-  
-}
-</script>
 <style>
 @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&subset=latin-ext');
 @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
